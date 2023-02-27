08187a52bc6b00326ccb0babf2318a66c7990bca
pengjingzheng <competep@163.com>
2023-02-27 10:04:37 +0800

    second commit~~

---
+++ src/main/java/com/example/demo/controller/TestGItController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestGItController {
+
+    @RequestMapping("/test/git")
+    public void gitTest() {
+        System.out.println("git测试!");
+    }
+
+
+}
