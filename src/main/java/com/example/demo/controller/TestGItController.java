56096e1b14e1022271cf670b61642843b4c3f2a5
pengjingzheng <competep@163.com>
2023-03-02 16:19:32 +0800

    分支 commit7

--- src/main/java/com/example/demo/controller/TestGItController.java
+++ src/main/java/com/example/demo/controller/TestGItController.java
@@ -10,19 +10,4 @@ public class TestGItController {
     public void gitTest() {
         System.out.println("git" +"test1");
     }
-    public void gitTest2() {
-        System.out.println("git" +"test2");
-    }
-    public void gitTest4() {
-        System.out.println("git" +"test4");
-    }
-    public void gitTest5() {
-        System.out.println("git" +"test5");
-    }
-    public void gitTest6() {
-        System.out.println("git" +"test6");
-    }
-    public void gitTest7() {
-        System.out.println("git" +"test7");
-    }
 }
