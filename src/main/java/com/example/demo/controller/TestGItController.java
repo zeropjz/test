5332cbddcfe484a3693add505f34731f77885635
pengjingzheng <competep@163.com>
2023-03-02 15:57:49 +0800

    分支 commit~~

--- src/main/java/com/example/demo/controller/TestGItController.java
+++ src/main/java/com/example/demo/controller/TestGItController.java
@@ -8,14 +8,7 @@ public class TestGItController {
 
     @RequestMapping("/test/git")
     public void gitTest() {
-        System.out.println("git测试!");
-        System.out.println("git分支测试!");
-          System.out.println("git分支测试2!");
-         System.out.println("git分支测试3!");
-        System.out.println("git分支测试4!");
-
-
-        System.out.println("git分支测试5 --分支合并!");
+        System.out.println("git" +"test1");
     }
 
 
